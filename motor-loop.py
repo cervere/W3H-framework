4882aabdd4b8b0850370fa0bfbd1302b911afe66
cervere <bhargav.rrv@gmail.com>
2017-11-08 15:45:10 +0100

    structures defined

--- motor-loop.py
+++ motor-loop.py
@@ -1,4 +1,10 @@
 import numpy as np
 
 
-VT = np.zeros(4)
+VT = np.zeros(4) # Just the representative feature of each stimulus. For eg : stim1 = ("food" : 8, "water" : 1)
+
+MMA = np.zeros(4) # The motor command that will lead to respective stimulus in VT. For eg : mot1 = {"yaw" : 45, move : 1, "expected" : (x,y)}
+
+PC = # gives current location - (x,y)
+
+BG = np.zeros(4)
